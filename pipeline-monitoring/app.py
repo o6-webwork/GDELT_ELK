893b551d129e148ea63069a0aba2514a23cfcb2b
The Prunerator <yeeteck05@gmail.com>
2025-03-25 11:14:54 +0800

    Made the Flask app working now

--- pipeline-monitoring/app.py
+++ pipeline-monitoring/app.py
@@ -7,11 +7,11 @@ current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 @app.route('/')
 def dashboard():
-    log_dir = ".\\app\\logs\\log.txt"
+    log_dir = "./logs/log.txt"
 
     with open(log_dir, "r") as f:
         data = f.read()
     return render_template("dashboard.html", data=data)
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
